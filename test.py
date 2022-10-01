011296241851bc0f1aba44092150cf811de2894e
magarets <alswn81616@gmail.com>
2022-10-02 02:04:54 +0900

    feat: Parse hyperLink in html

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hi")
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = 'https://www.naver.com' # url
+html = urlopen(url)
+bs = BeautifulSoup(html, 'html.parser')
+
+for link in bs.findAll('a'):
+    if 'href' in link.attrs: # find Hyperlink
+        print(link.attrs['href'])
