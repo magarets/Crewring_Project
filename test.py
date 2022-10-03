46fcff2db2bb54bc5c7805028cfbdea36d9c090e
magarets <alswn81616@gmail.com>
2022-10-04 00:52:38 +0900

    feat: add response func

--- test.py
+++ test.py
@@ -1,10 +1,29 @@
+import requests
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = 'https://www.naver.com' # url
-html = urlopen(url)
-bs = BeautifulSoup(html, 'html.parser')
+def crawlingHyperlink(url):
+    
+    html = urlopen(url)
+    bs = BeautifulSoup(html, 'html.parser')
 
-for link in bs.findAll('a'):
-    if 'href' in link.attrs: # find Hyperlink
-        print(link.attrs['href'])
+    for link in bs.findAll('a'):
+        if 'href' in link.attrs: # find Hyperlink
+            print(link.attrs['href'])
+            
+def crawlingWeb(url):
+    res = requests.get(url)
+    
+    if res.status_code == 200:
+        print(res.text)
+        
+
+def main():
+    
+    url = 'https://www.naver.com' # url
+    #crawlingHyperlink(url)
+    crawlingWeb(url)
+
+if __name__ == '__main__':
+    main()
